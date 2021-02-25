aa7cae38003a5f723f5fea101eab4e05fc2dad6c
Cyril Bravo <cyril.bravo@maximusbc.ca>
2021-02-25 10:16:59 -0800

    Generated new version for SiteReg

--- sitereg/src/version.GENERATED.ts
+++ sitereg/src/version.GENERATED.ts
@@ -1,9 +1,10 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '9b71bca';
-export const buildTime = '2/11/2021 at 10:10:12 AM';
+export const gitCommit = 'e187086';
+export const buildTime = '2/25/2021 at 10:12:36 AM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.0.0';
+export const projectVersion = '3.0.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 9b71bca - 2/11/2021 at 10:10:12 AM (v2.0.0) \nhttps://github.com/bcgov/moh-sitereg/commits/9b71bca';
+export const message = 'Canonical Version: e187086 - 2/25/2021 at 10:12:36 AM (v3.0.0) ' +
+'\nhttps://github.com/bcgov/MOH-IAM-Enrolment/commits/e187086';
 /** If true, other values should be present. */
 export const success = true;
